c172ef6ac1d3ace69929da83b43be7a17b78d779
Swif7Bl8ze <83766675+codeprabhu@users.noreply.github.com>
2026-08-28 12:55:23 +0530

    Add: open-the-lock - submission-1

---
+++ Data Structures & Algorithms/open-the-lock/submission-1.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    int openLock(vector<string>& deadends, string target) {
+        int n = deadends.size();
+        int m = target.size();
+        unordered_map<string, int> mp;
+        for(string deadend: deadends)
+        {
+            if(deadend == "0000")
+                return -1;
+
+            mp[deadend]++;
+        }   
+        if(target == "0000")
+            return 0;
+
+        queue<pair<string, int>> q;
+        q.push({"0000", 0});
+        
+        unordered_map<string, int> seen;
+        seen["0000"] = 1;
+        
+        while(!q.empty())
+        {
+            auto [code, turn] = q.front();
+            q.pop();
+
+            if(code == target)
+                return turn;
+
+            for(int i = 0; i < m; i++)
+            {
+                string newer = code;
+                if(newer[i] == '0')
+                    newer[i] = '9';
+                else
+                    newer[i] -= 1;
+
+                if(seen[newer])
+                    continue;
+                if(mp[newer])
+                    continue;
+                    
+                q.push({newer, turn+1});
+                seen[newer]++;
+            }
+
+            for(int i = 0; i < m; i++)
+            {
+                string newer = code;
+                if(newer[i] == '9')
+                    newer[i] = '0';
+                else
+                    newer[i] += 1;
+
+                if(seen[newer])
+                    continue;
+                if(mp[newer])
+                    continue;
+
+                q.push({newer, turn+1});
+                seen[newer]++;
+            }
+        }
+
+        return -1;
+    }
+};
